e1fa2d2976d69c630e355f10bea82eb0ee3968f6
Thomas Z Lukoma <tzlukoma@gmail.com>
2022-01-31 23:27:36 -0500

    initial scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+interface Session {
+  date: string,
+  attendees: string[]
+}
+
+interface WeekOfSessions {
+  weekEnding: string,
+  sessions: Session[]
+}
+
+type History = WeekOfSessions[]
+
+const auditHistory: History = [
+  {
+    weekEnding: '01/30/2022',
+    sessions: [
+      {
+        date: '01/24/2022',
+        attendees: [
+          'Endasi'
+        ]
+      },
+      {
+        date: '01/26/2022',
+        attendees: [
+          'Endasi'
+        ]
+      },
+      {
+        date: '01/30/2022',
+        attendees: [
+          'Ssanyu', 'Neo'
+        ]
+      }
+    ]
+  }
+]
+
+interface SessionProps {
+  session: Session
+}
+
+const SessionSegment = ({ session }: SessionProps) => {
+
+
+
+  return (
+    <div className={'border-2 bg-slate-100 p-2 text-center'}>
+      <div className={'text-2xl'}>{`${session.date}`}</div>
+      <div className={'text-4xl pt-5'}>{session.attendees.length}</div>
+      <div className={'flex justify-center'}>
+        {
+          session.attendees.map(attendee => {
+            return <span className={'p-5 text-2xl'}>{attendee}</span>
+          })
+        }
+      </div>
+
+    </div>
+
+  )
+}
 
 function App() {
+  console.log(auditHistory)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="w-1/2 m-auto p-10">
+      <h1 className="text-3xl font-bold">
+        Coach Q Training Tracker
+      </h1>
+      <br />
+      <div>
+        {
+          auditHistory.map(history => {
+            return (
+              <div>
+                <h2 className={'text-2xl mb-1'}>{`Week Ending ${history.weekEnding}`}</h2>
+                <div className="grid grid-cols-3 gap-1">
+                  {
+                    history.sessions.map(session => {
+                      return <div ><SessionSegment session={session} /></div>
+                    })
+                  }
+                </div>
+
+              </div>
+            )
+
+          })
+        }
+      </div >
+    </div >
   );
 }
 
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCz_eX8MhDCNWh5nUh80kQlN9sR4xP5Dg8",
+    authDomain: "coachq-tracker-data.firebaseapp.com",
+    projectId: "coachq-tracker-data",
+    storageBucket: "coachq-tracker-data.appspot.com",
+    messagingSenderId: "376332854288",
+    appId: "1:376332854288:web:c1135592af4dd936701695"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
